9bacff57a7d56339589d7204dc04203361f17cc7
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-09-16 11:46:19 +0200

    Updated models verbose_name

--- charity/models.py
+++ charity/models.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+import datetime
 
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
-from django.utils.translation import gettext_lazy
+from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
 zip_code_regex = RegexValidator(
@@ -19,11 +19,11 @@ min_bag_quantity = MinValueValidator(
     message='Ilość oddanych worków musi być większa od 0',
 )
 min_working_hours = MinValueValidator(
-    limit_value=datetime.time(8, 00, 00),
+    limit_value=datetime.time(hour=8, minute=00, second=00),
     message='Odbiór można zlecić w godzinach 8:00 - 20:00',
 )
 max_working_hours = MinValueValidator(
-    limit_value=datetime.time(20, 00, 00),
+    limit_value=datetime.time(hour=20, minute=00, second=00),
     message='Odbiór można zlecić w godzinach 8:00 - 20:00',
 )
 
@@ -39,14 +39,14 @@ tomorrow_date = MinValueValidator(
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=64, unique=True, verbose_name=gettext_lazy('name'))
+    name = models.CharField(max_length=64, unique=True, verbose_name=_('name'))
 
     def __str__(self):
         return f'{self.name}'
 
     class Meta:
         verbose_name = 'Kategoria'
-    #     verbose_name_plural = 'Kategorie'
+        verbose_name_plural = 'Kategorie'
 
 
 class Institution(models.Model):
@@ -58,9 +58,15 @@ class Institution(models.Model):
         (NGO, 'Organizacja pozarządowa'),
         (LOCAL_COLLECTION, 'Lokalna zbiórka'),
     ]
-    name = models.CharField(max_length=64, unique=True)
-    description = models.CharField(max_length=256)
-    type = models.CharField(max_length=3, choices=TYPE_OF_INSTITUTION, default=FOUNDATION)
+    name = models.CharField(max_length=64,
+                            unique=True,
+                            verbose_name=_('name'))
+    description = models.CharField(max_length=256,
+                                   verbose_name=_('description'))
+    type = models.CharField(max_length=3,
+                            choices=TYPE_OF_INSTITUTION,
+                            default=FOUNDATION,
+                            verbose_name=_('type'))
     categories = models.ManyToManyField(Category)
 
     def __str__(self):
@@ -69,21 +75,39 @@ class Institution(models.Model):
 
     class Meta:
         verbose_name = 'Instytucja'
-    #     verbose_name_plural = 'Instytucje'
+        verbose_name_plural = 'Instytucje'
 
 
 class Donation(models.Model):
-    quantity = models.IntegerField(validators=[min_bag_quantity])
-    street = models.CharField(max_length=256)
-    city = models.CharField(max_length=64)
-    zip_code = models.CharField(max_length=6, validators=[zip_code_regex])
+    quantity = models.IntegerField(validators=[min_bag_quantity],
+                                   verbose_name=_('quantity'),)
+    street = models.CharField(max_length=256,
+                              verbose_name=_('street'),)
+    city = models.CharField(max_length=64,
+                            verbose_name=_('city'),)
+    zip_code = models.CharField(max_length=6,
+                                validators=[zip_code_regex],
+                                verbose_name=_('zip code'),)
     # Could use PhoneNumberField instead
-    phone_number = models.CharField(max_length=15, validators=[phone_regex])
-    pick_up_date = models.DateField(validators=[tomorrow_date])
-    pick_up_time = models.TimeField(validators=[min_working_hours, max_working_hours])
-    pick_up_comment = models.CharField(max_length=256, blank=True, null=True)
-    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+    phone_number = models.CharField(max_length=15,
+                                    validators=[phone_regex],
+                                    verbose_name=_('phone number'),)
+    pick_up_date = models.DateField(validators=[tomorrow_date],
+                                    verbose_name=_('pick up date'),)
+    pick_up_time = models.TimeField(validators=[min_working_hours, max_working_hours],
+                                    verbose_name=_('pick up time'),)
+    pick_up_comment = models.CharField(max_length=256,
+                                       blank=True,
+                                       null=True,
+                                       verbose_name=_('comment'),)
+    institution = models.ForeignKey(Institution,
+                                    on_delete=models.CASCADE,
+                                    verbose_name=_('institution'),)
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             null=True,
+                             default=None,
+                             verbose_name=_('user'))
     categories = models.ManyToManyField(Category)
 
     def __str__(self):
@@ -92,4 +116,4 @@ class Donation(models.Model):
 
     class Meta:
         verbose_name = 'Dotacja'
-    #     verbose_name_plural = 'Dotacje'
+        verbose_name_plural = 'Dotacje'
