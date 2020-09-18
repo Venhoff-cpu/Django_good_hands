c53a6bc09d0cc775766936b789ca1782c9a6f0fa
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-09-18 11:43:15 +0200

    Revert changes to pagination

--- charity/views.py
+++ charity/views.py
@@ -15,19 +15,19 @@ class LandingPage(TemplateView):
         num_of_bags = Donation.objects.all().aggregate(Sum('quantity'))
         all_institutions = Institution.objects.all()
 
-        fou_paginator = Paginator(all_institutions.filter(type='FUN'), 5)
-        ngos_paginator = Paginator(all_institutions.filter(type='NGO'), 5)
-        local_paginator = Paginator(all_institutions.filter(type='LOC'), 5)
-
-        page_fou = self.request.GET.get('page_fou')
-        page_ngo = self.request.GET.get('page_ngo')
-        page_local = self.request.GET.get('page_local')
+        # fou_paginator = Paginator(all_institutions.filter(type='FUN'), 5)
+        # ngos_paginator = Paginator(all_institutions.filter(type='NGO'), 5)
+        # local_paginator = Paginator(all_institutions.filter(type='LOC'), 5)
+        #
+        # page_fou = self.request.GET.get('page_fou')
+        # page_ngo = self.request.GET.get('page_ngo')
+        # page_local = self.request.GET.get('page_local')
 
         ctx['organizations'] = helped_organizations
         ctx['bags'] = num_of_bags['quantity__sum']
-        ctx['foundations'] = fou_paginator
-        ctx['ngos'] = ngos_paginator.get_page(page_ngo)
-        ctx['local_collections'] = local_paginator.get_page(page_local)
+        ctx['foundations'] = all_institutions.filter(type='FUN')
+        ctx['ngos'] = all_institutions.filter(type='NGO')
+        ctx['local_collections'] = all_institutions.filter(type='LOC')
         return ctx
 
 
