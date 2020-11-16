15fb8b7f03db93cea1abbe717bcdaf624de8be21
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2020-11-16 14:17:13 +0100

    custom messages

--- Good_hands/settings/base.py
+++ Good_hands/settings/base.py
@@ -1,6 +1,7 @@
 import os
 import environ
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 
 root = environ.Path(__file__) - 3  # get root of the project
@@ -67,6 +68,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "Good_hands.wsgi.application"
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
